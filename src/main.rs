a7dad53a490cf0f5fe07434a58c66929ffaf282b
marclie <marcel.lieb@gmx.de>
2023-03-17 15:40:41 +0100

    Moved stream creation to utils/audiodevices.rs

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,11 @@
-use cpal::{self, traits::{HostTrait, DeviceTrait, StreamTrait}, Sample};
-use dasp_sample::ToSample;
+mod utils;
+
+use cpal::{traits::StreamTrait};
+use crate::utils::audiodevices::create_default_output_stream;
+
 
 fn main() {
-    let _hosts = cpal::available_hosts();
-    let default_host = cpal::default_host();
-    
-    let out = default_host.default_output_device().expect("no output device available");
-    let audio_cfg = out
-        .default_output_config()
-        .expect("No default output config found");
-
-    let _outstream = match audio_cfg.sample_format() {
-        cpal::SampleFormat::F32 => match out.build_input_stream(
-            &audio_cfg.config(),
-            move |data, _: &_| print_data::<f32>(data),
-            capture_err_fn,
-            None,
-        ) {
-            Ok(stream) => Some(stream),
-            Err(e) => {
-                panic!("{:?}", e)
-            }
-        },
-        cpal::SampleFormat::I16 => {
-            match out.build_input_stream(
-                &audio_cfg.config(),
-                move |data, _: &_| print_data::<i16>(data),
-                capture_err_fn,
-                None,
-            ) {
-                Ok(stream) => Some(stream),
-                Err(e) => {
-                    panic!("{:?}", e)
-                }
-            }
-        }
-        cpal::SampleFormat::U16 => {
-            match out.build_input_stream(
-                &audio_cfg.config(),
-                move |data, _: &_| print_data::<u16>(data),
-                capture_err_fn,
-                None,
-            ) {
-                Ok(stream) => Some(stream),
-                Err(e) => {
-                    panic!("{:?}", e)
-                }
-            }
-        }
-        _ => None,
-    } .unwrap();
-    _outstream.play().unwrap();
-    println!("Default output device: {:?}", out.name().unwrap());
-    println!("Default output sample format: {:?}", audio_cfg.sample_format());
-    println!("Default output buffer size: {:?}", audio_cfg.buffer_size());
-    println!("Default output sample rate: {:?}", audio_cfg.sample_rate());
-    println!("Default output channels: {:?}", audio_cfg.channels());
-    //println!("Stream was created: {}", outstream.is_some());
-    std::thread::sleep(std::time::Duration::from_millis(20000));
-
-    println!("Stream was dropped");
-}
-
-fn print_data<T>(data: &[T])
-where T: Sample + ToSample<f32> {
-    //println!("Frame length: {}", data.len());
-    let sound = data.iter().any(|i| *i != Sample::EQUILIBRIUM);
-    let volume: f32 = data.iter().fold(0.0, |acc, e:&T| acc +  T::to_sample::<f32>(*e).abs()) / data.len() as f32;
-    let peak = data.iter().map(|s| T::to_sample::<f32>(*s)).into_iter().fold(-1.0,|max, f| if f > max {f} else {max});
-    if sound {
-        println!("RMS: {:.3}, Peak: {:.3}", volume, peak);
-    }
-}
-
-fn capture_err_fn(err: cpal::StreamError) {
-    eprintln!("an error occurred on stream: {}", err);
+    let stream = create_default_output_stream();
+    stream.play().unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
