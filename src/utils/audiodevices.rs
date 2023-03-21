075ab71bfcc69b994f50b6d23cc140ef0c30b940
Marclie <marclie1@gmx.de>
2023-03-21 14:19:51 +0100

    prepared for fft

--- src/utils/audiodevices.rs
+++ src/utils/audiodevices.rs
@@ -1,7 +1,10 @@
-use cpal::{self, traits::{HostTrait, DeviceTrait}, Sample};
+use std::vec;
+
+use cpal::{self, traits::{HostTrait, DeviceTrait}, Sample, StreamConfig};
 use dasp_sample::ToSample;
-use dasp_ring_buffer::Bounded;
-use rustfft::{FftPlanner, num_complex::ComplexFloat};
+use realfft::RealFftPlanner;
+//use rustfft::num_complex::Complex;
+//use rustfft::num_traits::Zero;
 use log::debug;
 
 
@@ -9,18 +12,37 @@ fn capture_err_fn(err: cpal::StreamError) {
     eprintln!("an error occurred on stream: {}", err);
 }
 
-fn print_data<T>(data: &[T])
+fn print_data<T>(data: &[T], f32_samples: &mut Vec<f32>)
 where T: Sample + ToSample<f32> {
-    //println!("Frame length: {}", data.len());
-    let sound = data.iter().any(|i| *i != Sample::EQUILIBRIUM);
-    let volume: f32 = (data
-        .iter().fold(0.0, |acc, e:&T| acc +  T::to_sample::<f32>(*e) * T::to_sample::<f32>(*e)) / data.len() as f32).sqrt();
-    let peak = data
-        .iter().map(|s| T::to_sample::<f32>(*s))
-        .into_iter().fold(0.0,|max, f| if f.abs() > max {f.abs()} else {max});
+    f32_samples.clear();
+    f32_samples.extend(data.iter().map(|x: &T| T::to_sample::<f32>(*x)));
+    let buffer_size = f32_samples.len();
+
+    f32_samples.extend(vec![0.0; f32_samples.capacity() - f32_samples.len()]);
+    // println!("Frame length: {}", buffer_size);
+
+    // Calculate RMS and peak volume
+    let sound = f32_samples.iter().any(|i| *i != Sample::EQUILIBRIUM);
+    let volume: f32 = (f32_samples
+        .iter().fold(0.0, |acc, e| acc +  e * e) / buffer_size as f32).sqrt();
+    let peak = f32_samples
+        .iter().fold(0.0,|max, f| if f.abs() > max {f.abs()} else {max});
     if sound {
         println!("RMS: {:.3}, Peak: {:.3}", volume, peak);
     }
+
+
+    let mut planner = RealFftPlanner::<f32>::new();
+    let fft = planner.plan_fft_forward(f32_samples.capacity());
+
+    let mut output = fft.make_output_vec();
+    match fft.process(f32_samples, &mut output) {
+        Ok(_) => (),
+        Err(e) => println!("Error: {:?}", e)
+    }
+
+    let output = output.iter().map(|e| e.re.abs()).collect::<Vec<f32>>();
+
 }
 
 pub fn create_default_output_stream() -> cpal::Stream {
@@ -32,10 +54,11 @@ pub fn create_default_output_stream() -> cpal::Stream {
         .default_output_config()
         .expect("No default output config found");
 
+    let mut f32_samples: Vec<f32> = Vec::with_capacity(22050);
     let outstream = match audio_cfg.sample_format() {
         cpal::SampleFormat::F32 => match out.build_input_stream(
             &audio_cfg.config(),
-            move |data: &[f32], _| print_data(data),
+            move |data: &[f32], _| print_data(data, &mut f32_samples),
             capture_err_fn,
             None,
         ) {
@@ -47,7 +70,7 @@ pub fn create_default_output_stream() -> cpal::Stream {
         cpal::SampleFormat::I16 => {
             match out.build_input_stream(
                 &audio_cfg.config(),
-                move |data: &[i16], _| print_data(data),
+                move |data: &[i16], _| print_data(data, &mut f32_samples),
                 capture_err_fn,
                 None,
             ) {
@@ -60,7 +83,7 @@ pub fn create_default_output_stream() -> cpal::Stream {
         cpal::SampleFormat::U16 => {
             match out.build_input_stream(
                 &audio_cfg.config(),
-                move |data: &[u16], _| print_data(data),
+                move |data: &[u16], _| print_data(data, &mut f32_samples),
                 capture_err_fn,
                 None,
             ) {
@@ -78,4 +101,15 @@ pub fn create_default_output_stream() -> cpal::Stream {
     debug!("Default output sample rate: {:?}", audio_cfg.sample_rate());
     debug!("Default output channels: {:?}", audio_cfg.channels());
     outstream.unwrap()
+}
+
+fn split_channels<T> (config: &StreamConfig, data: &[T]) -> Vec<Vec<f32>> 
+where T: Sample + ToSample<f32> {
+    let samples: Vec<f32> = data.iter().map(|s| s.to_sample::<f32>()).collect();
+    let channels = config.channels;
+    let mut out: Vec<Vec<f32>> = Vec::new();
+    for i in 0..channels {
+        out.push(samples.iter().enumerate().filter_map(|(index, f)| if index as u16 % channels == i {Some(*f)} else {None}).collect())
+    }
+    out
 }
