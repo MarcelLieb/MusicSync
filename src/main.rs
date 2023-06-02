a19882bc5ad55428f99b8857ddffeb3d859f81e6
Marcel Lieb <marclie1@gmx.de>
2023-06-02 11:59:10 +0200

    improved syntax

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,18 @@ use cpal::traits::StreamTrait;
 
 #[tokio::main]
 async fn main() {
-    let stream = create_default_output_stream();
-    stream.play().unwrap();
-    let (tx, rx) = channel();
+    {
+        let stream = create_default_output_stream();
+        stream.play().unwrap();
+        let (tx, rx) = channel();
 
-    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
-        .expect("Error setting Ctrl-C handler");
+        ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+            .expect("Error setting Ctrl-C handler");
 
-    println!("Stop sync with CTRL-C");
-    rx.recv().expect("Could not receive from channel.");
-    println!("Shutting down");
-    drop(stream);
+        println!("Stop sync with CTRL-C");
+        rx.recv().expect("Could not receive from channel.");
+        println!("Shutting down");
+    }
 
     let file = File::open("onsets.cbor").expect("Couldn't open file");
     let data: HashMap<String, Vec<(u128, Event)>> = from_reader(file).unwrap();
