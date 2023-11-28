0b0734d15943047a1b25a152ed18aaf25be9b925
Marcel Lieb <marclie1@gmx.de>
2023-11-28 23:10:10 +0100

    parametrized stream creation

--- src/utils/audiodevices.rs
+++ src/utils/audiodevices.rs
@@ -1,4 +1,3 @@
-use crate::utils::audioprocessing::hfc::Hfc;
 use crate::utils::audioprocessing::spectral_flux::SpecFlux;
 use crate::utils::audioprocessing::ProcessingSettings;
 use crate::utils::audioprocessing::{prepare_buffers, process_raw};
@@ -11,31 +10,92 @@ use cpal::{
 };
 use log::debug;
 
+use super::audioprocessing::spectral_flux::SpecFluxSettings;
+
 fn capture_err_fn(err: cpal::StreamError) {
     eprintln!("an error occurred on stream: {}", err);
 }
 
-pub async fn create_default_output_stream() -> cpal::Stream {
-    let _hosts = cpal::available_hosts();
-    let default_host = cpal::default_host();
+pub fn create_monitor_stream(device_name: &str, processing_settings: ProcessingSettings, detection_settings: SpecFluxSettings, lightservices: Vec<Box<dyn LightService + Send>>) -> Result<cpal::Stream, BuildStreamError> {
+    let out = cpal::default_host()
+        .devices()
+        .map_err(|_| BuildStreamError::DeviceNotAvailable)?
+        .filter(|d| d.name().unwrap_or_default() == device_name)
+        .next()
+        .ok_or_else(|| BuildStreamError::DeviceNotAvailable)?;
 
-    let out = default_host
-        .default_output_device()
-        .expect("no output device available");
     let audio_cfg = out
         .default_output_config()
         .expect("No default output config found");
 
     let channels = audio_cfg.channels();
 
-    let settings = ProcessingSettings::default();
     let config = StreamConfig {
         channels,
-        sample_rate: cpal::SampleRate(settings.sample_rate),
+        sample_rate: cpal::SampleRate(processing_settings.sample_rate),
         buffer_size: cpal::BufferSize::Default,
     };
 
-    let mut detection_buffer = prepare_buffers(channels, &settings);
+    let mut lightservices = lightservices;
+
+    let mut detection_buffer = prepare_buffers(channels, &processing_settings);
+
+    let mut spec_flux = SpecFlux::with_settings(processing_settings.sample_rate, processing_settings.fft_size as u32, detection_settings);
+
+    let buffer_size = processing_settings.buffer_size * channels as usize;
+    let hop_size = processing_settings.hop_size * channels as usize;
+    macro_rules! build_buffered_onset_stream {
+        ($t:ty) => {{
+            let mut buffer: Vec<$t> = Vec::new();
+
+            out.build_input_stream(
+                &config,
+                move |data: &[$t], _| {
+                    buffer.extend(data);
+                    let n = (buffer.len() + hop_size).saturating_sub(buffer_size) / hop_size;
+
+                    (0..n).for_each(|_| {
+                        process_raw(&buffer[0..buffer_size], channels, &mut detection_buffer);
+
+                        let onsets = spec_flux.detect(
+                            &detection_buffer.freq_bins,
+                            detection_buffer.peak,
+                            detection_buffer.rms,
+                        );
+                        lightservices.process_onsets(&onsets);
+                        lightservices.process_spectrum(&detection_buffer.freq_bins);
+                        lightservices.process_samples(&detection_buffer.mono_samples);
+                        lightservices.update();
+
+                        buffer.drain(0..hop_size);
+                    })
+                },
+                capture_err_fn,
+                None,
+            )
+        }};
+    }
+    let outstream = match audio_cfg.sample_format() {
+        cpal::SampleFormat::F32 => build_buffered_onset_stream!(f32),
+        cpal::SampleFormat::I16 => build_buffered_onset_stream!(i16),
+        cpal::SampleFormat::U16 => build_buffered_onset_stream!(u16),
+        _ => Err(BuildStreamError::StreamConfigNotSupported),
+    };
+    debug!("Default output device: {:?}", out.name().unwrap());
+    debug!(
+        "Default output sample format: {:?}",
+        audio_cfg.sample_format()
+    );
+    debug!("Default output buffer size: {:?}", audio_cfg.buffer_size());
+    debug!("Default output sample rate: {:?}", audio_cfg.sample_rate());
+    debug!("Default output channels: {:?}", audio_cfg.channels());
+    outstream
+}
+
+pub async fn create_default_output_stream() -> Result<cpal::Stream, BuildStreamError> {
+    let device = cpal::default_host().default_output_device().ok_or_else(|| BuildStreamError::DeviceNotAvailable)?;
+
+    let settings = ProcessingSettings::default();
 
     let mut lightservices: Vec<Box<dyn LightService + Send>> = Vec::new();
     if let Ok(bridge) = hue::connect().await {
@@ -62,65 +122,6 @@ pub async fn create_default_output_stream() -> cpal::Stream {
         settings.hop_size,
     );
     lightservices.push(Box::new(serializer));
-
-    let mut spec_flux = SpecFlux::init(settings.sample_rate, settings.fft_size as u32);
-
-    let mut _hfc = Hfc::init(settings.sample_rate as usize, settings.fft_size);
-
-    let buffer_size = settings.buffer_size * channels as usize;
-    let hop_size = settings.hop_size * channels as usize;
-    macro_rules! build_buffered_onset_stream {
-        ($t:ty) => {{
-            let mut buffer: Vec<$t> = Vec::new();
-
-            out.build_input_stream(
-                &config,
-                move |data: &[$t], _| {
-                    buffer.extend(data);
-                    let n = (buffer.len() + hop_size).saturating_sub(buffer_size) / hop_size;
-
-                    (0..n).for_each(|_| {
-                        process_raw(&buffer[0..buffer_size], channels, &mut detection_buffer);
-
-                        let onsets = spec_flux.detect(
-                            &detection_buffer.freq_bins,
-                            detection_buffer.peak,
-                            detection_buffer.rms,
-                        );
-                        lightservices.process_onsets(&onsets);
-                        lightservices.process_spectrum(&detection_buffer.freq_bins);
-                        lightservices.process_samples(&detection_buffer.mono_samples);
-                        lightservices.update();
-                        /*
-                        let onsets = _hfc.detect(
-                            &detection_buffer.freq_bins,
-                            detection_buffer.peak,
-                            detection_buffer.rms
-                        );
-                         */
-
-                        buffer.drain(0..hop_size);
-                    })
-                },
-                capture_err_fn,
-                None,
-            )
-        }};
-    }
-    let outstream = match audio_cfg.sample_format() {
-        cpal::SampleFormat::F32 => build_buffered_onset_stream!(f32),
-        cpal::SampleFormat::I16 => build_buffered_onset_stream!(i16),
-        cpal::SampleFormat::U16 => build_buffered_onset_stream!(u16),
-        _ => Err(BuildStreamError::StreamConfigNotSupported),
-    }
-    .expect("Couldn't build input stream.\nMake sure you are running at 48kHz sample rate");
-    debug!("Default output device: {:?}", out.name().unwrap());
-    debug!(
-        "Default output sample format: {:?}",
-        audio_cfg.sample_format()
-    );
-    debug!("Default output buffer size: {:?}", audio_cfg.buffer_size());
-    debug!("Default output sample rate: {:?}", audio_cfg.sample_rate());
-    debug!("Default output channels: {:?}", audio_cfg.channels());
-    outstream
+    let detection_settings = SpecFluxSettings::default();
+    create_monitor_stream(&device.name().unwrap_or_default(), settings, detection_settings, lightservices)
 }
