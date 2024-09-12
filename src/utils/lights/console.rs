d6422b318444589633d548e6374790d5067ae039
Marclie <marclie1@gmx.de>
2024-09-12 16:29:11 +0200

    update to use new variant

--- src/utils/lights/console.rs
+++ src/utils/lights/console.rs
@@ -14,7 +14,7 @@ impl LightService for Console {
             Onset::Kick(s) => self.output[0] = "■".repeat((s * 9.0).ceil() as usize).bright_red(),
             Onset::Hihat(s) => self.output[1] = "■".repeat((s * 9.0).ceil() as usize).white(),
             Onset::Full(s) => self.output[2] = "■".repeat((s * 9.0).ceil() as usize).cyan(),
-            Onset::Note(s, _) => self.output[3] = "■".repeat((s * 9.0).ceil() as usize).blue(),
+            Onset::Snare(s) => self.output[3] = "■".repeat((s * 9.0).ceil() as usize).blue(),
             Onset::Atmosphere(s, _) => {
                 self.output[4] = "-".repeat((s * 9.0).ceil() as usize).black();
             }
